996cc9a53a8ea6d2535cdcc09d43116317e50e80
kostryukovd <kostryukovd@gmail.com>
2018-11-15 17:37:58 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+
+function expand_menu() {
+
+
+$(".header-menu__lines").toggleClass("header-menu__lines-rotate");
+
+//$(".hidden-menu").slideToggle();
+
+$(".hidden-menu").toggleClass("hidden-menu__show");
+
+}
+
+
